973ee901b9a580947a3d66cbd04b08b14aeed28c
audioeffect <audioeffect@gmail.com>
2020-04-20 10:15:51 -0500

    Added Modules

---
+++ Modules/Out-Message.psm1
@@ -0,0 +1,41 @@
+﻿function Out-Message{
+    Param(
+        [string]$Message,
+        [string]$LogPath = $LogFile,
+        [switch]$NoTimestamp,
+        [switch]$NoAppend,
+        [switch]$LogOnly
+    )
+
+    if(!$NoTimestamp){
+        if(!$LogOnly){
+            if(!$NoAppend){
+                Write-Output ("{0} - {1}" -f (Get-Date), $Message) | Tee-Object $LogPath -Append
+            } else {
+                Write-Output ("{0} - {1}" -f (Get-Date), $Message) | Tee-Object $LogPath
+            }
+        } else {
+            if(!$NoAppend){
+                Write-Output ("{0} - {1}" -f (Get-Date), $Message) | Out-File $LogPath -Append
+            } else {
+                Write-Output ("{0} - {1}" -f (Get-Date), $Message) | Out-File $LogPath
+            }
+        }
+    } else {
+        if(!$LogOnly){
+            if(!$NoAppend){
+                Write-Output ("{0}" -f $Message) | Tee-Object $LogPath -Append
+            } else {
+                Write-Output ("{0}" -f $Message) | Tee-Object $LogPath
+            }
+        } else {
+            if(!$NoAppend){
+                Write-Output ("{0}" -f $Message) | Out-File $LogPath -Append
+            } else {
+                Write-Output ("{0}" -f $Message) | Out-File $LogPath
+            }
+        }
+    }
+}
+
+Export-ModuleMember -Function Out-Message
---
+++ Modules/Restart-Elevated.psm1
@@ -0,0 +1,50 @@
+﻿###################################
+#
+# Use the code block below in your 
+# script to import this module and 
+# elevate your script.
+#
+###################################
+<#
+
+# Import module and run script elevated
+if(!$Elevated){
+    Import-Module  "\\tsupport\tsupload\Cole\PS\Modules\Restart-Elevated.psm1"
+    Restart-Elevated -PassedCommand $PSCommandPath -PassedParam $PSBoundParameters
+}
+
+#>
+###################################
+#    Elevate and Launch Script    #
+###################################
+function Restart-Elevated([switch]$Elevated, $PassedCommand, $PassedParam){
+
+    $ParamList = ""
+
+    foreach($boundparam in $PassedParam.GetEnumerator()) {
+        $ParamList += " -{0} {1}" -f $boundparam.Key,$boundparam.Value
+    }
+
+    $MyCommand = $PassedCommand + $ParamList
+
+    $arguments = "-executionpolicy bypass -noprofile -file $MyCommand -elevated"
+
+    function Test-Admin {
+      $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+      $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    }
+
+    if ((Test-Admin) -eq $false)  {
+        if ($elevated) 
+        {
+            # tried to elevate, did not work, aborting
+        } 
+        else {
+            Start-Process powershell.exe -Verb RunAs -ArgumentList $arguments
+        }
+
+        exit
+    }
+}
+
+Export-ModuleMember -Function Restart-Elevated
